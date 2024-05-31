f56322f087be303598f2150e0855729dadbdc0e1
alevchenko12 <nastiya.levchenko@gmail.com>
2024-05-31 08:57:33 +0200

    Removed Secret Key

--- project_gallery/settings.py
+++ project_gallery/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)rjgz!)8hpqk#-3gua@da_qdvgg=t^#t)ftx-9v4n+@id#ca('
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOST = os.environ.get('ALLOWED_HOST', '*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
